5eda48d3604dc253517fa5123e0bd1635a9dc718
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-01-26 12:11:25 +0200

    Added MatrixOperations class, implemented its constructor and random generation of IntMatrix elements

---
+++ MatrixOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class MatrixOperations
+{
+    public int[,] IntMatrix { get; private set; }
+
+    public MatrixOperations(int rows, int columns)
+    {
+        Random rand = new Random();
+
+        IntMatrix = new int[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                IntMatrix[i, j] = rand.Next(-100, 100);
+            }
+        }
+    }
+}
